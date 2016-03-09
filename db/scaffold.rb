4442cbee930a842150938acca00adb1f98759151
Boss2bmj <boss2bmj@gmail.com>
2016-03-09 22:54:38 +0700

    before add car status id to image

--- db/scaffold.rb
+++ db/scaffold.rb
@@ -1,6 +1,9 @@
 rails g scaffold Car car_license:string description:text enter_date:date car_category:string car_subcategory:string
 
 rails g scaffold CarStatus description:text status:boolean
+rails g migration add_car_id_to_car_statuses car_id:integer
 rails g scaffold CarImage image_name:string
 
 rails g paperclip CarImage image
+
+rails g migration add_car_status_id_to_car_images car_id:integer
