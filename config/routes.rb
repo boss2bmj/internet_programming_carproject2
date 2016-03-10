72fc6d6e3aeb53232688aa7d2d93b60d3467a0bf
Boss2bmj <boss2bmj@gmail.com>
2016-03-10 13:42:58 +0700

    before we start

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :admins
 
   resources :cars do
-    resources :car_statuses, except:[:show, :index]
+    resources :car_statuses, except:[ :index]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
