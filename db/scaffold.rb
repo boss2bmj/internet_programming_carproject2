ba3b8a938582bd9fd55c2f87c1249c10a3daffd9
Boss2bmj <boss2bmj@gmail.com>
2016-03-17 14:47:11 +0700

    delete devise User already

--- db/scaffold.rb
+++ db/scaffold.rb
@@ -7,3 +7,8 @@ rails g scaffold CarImage image_name:string
 rails g paperclip CarImage image
 
 rails g migration add_car_status_id_to_car_images car_status_id:integer
+
+
+
+
+rails g scaffold User first_name:string last_name:string gender:text birth_date:date email:string mobile_number:integer password:text
