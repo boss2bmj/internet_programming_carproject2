8dcb482aeb3b765cb20cfd1a4272990b20b9c944
Boss2bmj <boss2bmj@gmail.com>
2016-03-19 01:22:49 +0700

    try

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :deploy_to, '/home/boss2bmj/Internet-programming-carservice'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :keep_releases, 3
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
 
 namespace :deploy do
   task :restart do
