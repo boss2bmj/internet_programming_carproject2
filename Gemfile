0ffacb42aa9ab365a558522c76c7c3225d9d4af8
Boss2bmj <boss2bmj@gmail.com>
2016-03-19 13:31:03 +0700

    ....

--- Gemfile
+++ Gemfile
@@ -30,10 +30,13 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 gem "paperclip", "~> 4.3"
 gem 'rails_admin'
+<<<<<<< HEAD
 gem 'pg'
 gem 'capistrano', '~> 3.4.0'
 gem 'capistrano-rails'
 gem 'capistrano-rvm'
+=======
+>>>>>>> parent of 085c6f2... before deploy
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160317081433) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -31,8 +28,8 @@ ActiveRecord::Schema.define(version: 20160317081433) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
   create_table "car_images", force: :cascade do |t|
     t.string   "image_name"
